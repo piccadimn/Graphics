01d9e013c744c104738a488f1316b9229215532f
MD. Chand Alam <csadpeb@gmail.com>
2017-03-03 11:24:34 +0530

    Update OpenGL.cpp

--- OpenGL.cpp
+++ OpenGL.cpp
@@ -12,11 +12,7 @@ void display()
 
     glBegin(GL_LINES);
 
-    glColor3f(0.0,0.0,1.0);
-    glVertex2f(0.0, 0.0);
-    glVertex2f(0.1, 0.2);
-
-     glColor3f(0.0,0.0,1.0);
+    glColor3f(0.0,0.0,1.0);  //clock tick
     glVertex2f(0.0, 0.0);
     glVertex2f(-0.12, 0.12);
 
@@ -29,25 +25,15 @@ void display()
    glVertex2f(-0.15, -0.6);
  glEnd();
 
-   glBegin(GL_LINE_LOOP);   // circle
-   glColor3f(0.0,1.0,0.0);
-    for(int i =0; i <= 300; i++)
+   glBegin(GL_LINES);   // circle
+   glColor3f(0.467, 0.533, 0.600);
+    for(int i =0; i <= 3000; i++)
      {
-      double angle = 2 * PI * i / 300;
-      double x = cos(angle);
-      double y = sin(angle);
-      glVertex2d(x/6,y/6);
-     }
-  glEnd();
-
-   glBegin(GL_LINE_LOOP);   // circle
-   glColor3f(0.0,1.0,0.0);
-    for(int i =0; i <= 300; i++)
-     {
-      double angle = 2 * PI * i / 300;
+      double angle = 2 * PI * i / 3000;
       double x = cos(angle);
       double y = sin(angle);
       glVertex2d(x/4,y/4);
+      glVertex2d(x/6,y/6);
      }
   glEnd();
 
@@ -55,22 +41,22 @@ void display()
 
     glColor3f(1.0,1.0,0.0);
     glVertex2f(0.0,0.0);
-    glVertex2f(0.0,-0.4);
+    glVertex2f(0.001,-0.68);
 
     glEnd();
 
 
    glBegin(GL_LINE_LOOP);
-    glColor3f(0.0,1.0,0.0);
+    glColor3f(0.000, 0.000, 1.000);
 
    for(int r=16;r<300;r++)       //bob
    {
-    for(int i =0; i <= 300; i++)
+    for(int i =0; i <= 3000; i++)
      {
-      double angle = 2 * PI * i / 300;
+      double angle = 2 * PI * i / 3000;
       double x = cos(angle);
       double y = sin(angle);
-      glVertex2d(0.0+x/r,-0.4+ y/r);
+      glVertex2d(0.001+ x/r,-0.68+ y/r);
      }
    }
    glEnd();
@@ -87,6 +73,45 @@ void display()
      }
   glEnd();
 
+  glPushMatrix();
+   glBegin(GL_LINES);
+   glColor3f(0.0,0.0,1.0);
+   glVertex3f(0.0,0.0,0.0);
+   glVertex3f(0.1,0.2,0.0);
+   glEnd();
+  glPopMatrix();
+
+  glPushMatrix();
+   glBegin(GL_LINES);
+    glColor3i(0,0,0);
+    glVertex2f(0.0,0.24);
+    glVertex2f(0.0,0.18);
+   glEnd();
+  glPopMatrix();
+
+  glPushMatrix();
+   glBegin(GL_LINES);
+    glColor3i(0,0,0);
+    glVertex2f(0.0,-0.24);
+    glVertex2f(0.0,-0.18);
+   glEnd();
+  glPopMatrix();
+
+  glPushMatrix();
+   glBegin(GL_LINES);
+    glColor3i(0,0,0);
+    glVertex2f(0.24,0.0);
+    glVertex2f(0.18,0.0);
+   glEnd();
+  glPopMatrix();
+
+   glPushMatrix();
+   glBegin(GL_LINES);
+    glColor3i(0,0,0);
+    glVertex2f(-0.24,0.0);
+    glVertex2f(-0.18,0.0);
+   glEnd();
+  glPopMatrix();
 
     glutSwapBuffers();
 }
